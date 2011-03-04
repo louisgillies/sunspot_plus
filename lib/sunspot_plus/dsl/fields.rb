6108d0ab9f12c80aeb882e2f8d0b309466561dc4
Louis Gillies <louisgillies@yahoo.co.uk>
2011-03-04 14:09:39 +0000

    alias method name for sort fields

--- lib/sunspot_plus/dsl/fields.rb
+++ lib/sunspot_plus/dsl/fields.rb
@@ -5,7 +5,7 @@ module Sunspot
       # Could use adjust_solr_params but not sure about this method.
       def sort_fields(*names, &block)
         options = names.pop if names.last.is_a?(Hash)
-        names.each{|name| case_insensitive_sort(Sunspot::Type::CaseInsensitiveSortType.instance.field_name(name), options, &block)}
+        names.each{|name| case_insensitive_sort(Sunspot::Type::CaseInsensitiveSortType.instance.field_name(name), {:as => name}, &block)}
       end
     end
   end
